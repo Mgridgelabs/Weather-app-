f8219001f6addc9c0081e31bf44f0c2d52e5859a
Mgridgelabs <mainaianndungu@gmail.com>
2025-01-29 15:42:21 +0300

    `Update dependencies to daisyui 5.0.0-beta.2 and remove unused packages`

--- weather-app/vite.config.js
+++ weather-app/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+// import daisyui from 'daisyui'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    // daisyui(), // Enables DaisyUI components and utilities in your project.
   ],
 })
